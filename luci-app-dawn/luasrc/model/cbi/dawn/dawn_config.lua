7c679ee773ea34e80fbed45560e3ec531654af7f
Polynomialdivision <vincent@systemli.org>
2018-08-03 20:27:44 +0200

    update

--- luci-app-dawn/luasrc/model/cbi/dawn/dawn_config.lua
+++ luci-app-dawn/luasrc/model/cbi/dawn/dawn_config.lua
@@ -1,5 +1,5 @@
 m = Map("dawn", translate("Dawn Configuration"), translate(""))
-s = m:section(TypedSection, "metric", "Metric", "Metric"); s.anonymous=true;
+s = m:section(TypedSection, "metric", "Metric", "Metric"); s.anonymous = true;
 s:option(Value, "ht_support", "High Throughput Support")
 s:option(Value, "no_ht_support", "No High Throughput Support")
 s:option(Value, "vht_support", "Very High Throughput Support")
@@ -11,7 +11,7 @@ s:option(Value, "chan_util", "Channel Utilization")
 s:option(Value, "max_chan_util", "Above Maximum Channel Utilization")
 s:option(Value, "min_probe_count", "Minimum Number of Probe Requests")
 
-s = m:section(TypedSection, "metric", "Threshold", "Thresholds"); s.anonymous=true;
+s = m:section(TypedSection, "metric", "Threshold", "Thresholds"); s.anonymous = true;
 s:option(Value, "bandwith_threshold", "Bandwidth Threshold")
 s:option(Value, "rssi_val", "RSSI Threshold")
 s:option(Value, "low_rssi_val", "Low RSSI Threshold")
@@ -20,14 +20,14 @@ s:option(Value, "max_chan_util_val", "Maximaum Channel Utilization Threshold")
 s:option(Value, "min_probe_count", "Minimum Probe Count")
 s:option(Value, "min_number_to_kick", "Minimum Number After Kicking Client")
 
-s = m:section(TypedSection, "metric", "Evaluate", "What should be evaluated?"); s.anonymous=true;
+s = m:section(TypedSection, "metric", "Evaluate", "What should be evaluated?"); s.anonymous = true;
 s:option(Flag, "kicking", "Activate Kicking")
 s:option(Flag, "eval_probe_req", "Evaluate Probe Requests")
 s:option(Flag, "eval_auth_req", "Evaluate Authentication Requests")
 s:option(Flag, "eval_assoc_req", "Evaluate Association Requests")
 s:option(Flag, "use_station_count", "Use Station Count")
 
-s = m:section(TypedSection, "metric", "IEE802.11", "Reasons for denying"); s.anonymous=true;
+s = m:section(TypedSection, "metric", "IEE802.11", "Reasons for denying"); s.anonymous = true;
 s:option(Value, "deny_auth_reason", "Denying Authentication")
 s:option(Value, "deny_assoc_reason", "Denying Association")
 
--- luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
+++ luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
@@ -8,49 +8,48 @@ function s.render(self, sid)
 	tpl.render_string([[
 		<ul>
 			<%
-				local utl = require "luci.util"
-				local stat = utl.ubus("dawn", "get_network", { })
-				local name, macs
-				for name, macs in pairs(stat) do
-					%>
-						<li>
-							<strong>SSID is: </strong><%= name %><br />
-						</li>
-						<ul>
-					<%
-					local mac, data
-					for mac, data in pairs(macs) do
-					%>
-						<li>
-							<strong>AP MAC is: </strong><%= mac %><br />
-						</li>
-						<ul>
-					<%
-						local mac2, data2
-						for mac2, data2 in pairs(data) do
+			local utl = require "luci.util"
+			local stat = utl.ubus("dawn", "get_network", { })
+			local name, macs
+			for name, macs in pairs(stat) do
 			%>
-			 
 				<li>
-		                        <strong>Client is: </strong><%= mac2 %><br />
-					<strong>Frequency is: </strong><%= "%.3f" %( data2.freq / 1024 ) %>GHz<br />
-                                        <strong>HT: </strong><%= (data2.ht == true) and "available" or "not available" %><br />  
-                                        <strong>VHT: </strong><%= (data2.vht == true) and "available" or "not available" %><br />  
-					<!--
-					<strong>Signal is: </strong><%= "%d" %data2.signal %><br />                                                                            
-					<strong>Channel Utilization is: </strong><%= "%d" %data2.channel_utilization %><br />
-                                        <strong>Station connected to AP is: </strong><%= "%d" %data2.num_sta %><br />
-                                        --!>
+					<strong>SSID is: </strong><%= name %><br />
 				</li>
-			<%
-						end
+				<ul>
+				<%
+				local mac, data
+				for mac, data in pairs(macs) do
+				%>
+					<li>
+						<strong>AP MAC is: </strong><%= mac %><br />
+					</li>
+					<ul>
+					<%
+					local mac2, data2
+					for mac2, data2 in pairs(data) do
 					%>
-					</ul>
+						<li>
+						<strong>Client is: </strong><%= mac2 %><br />
+						<strong>Frequency is: </strong><%= "%.3f" %( data2.freq / 1024 ) %>GHz<br />
+						<strong>HT: </strong><%= (data2.ht == true) and "available" or "not available" %><br />
+						<strong>VHT: </strong><%= (data2.vht == true) and "available" or "not available" %><br />
+						<!--
+						<strong>Signal is: </strong><%= "%d" %data2.signal %><br />
+						<strong>Channel Utilization is: </strong><%= "%d" %data2.channel_utilization %><br />
+						<strong>Station connected to AP is: </strong><%= "%d" %data2.num_sta %><br />
+						--!>
+						</li>
 					<%
 					end
-				%>
+					%>
 				</ul>
 				<%
 				end
+				%>
+			</ul>
+			<%
+			end
 			%>
 		</ul>
 	]])

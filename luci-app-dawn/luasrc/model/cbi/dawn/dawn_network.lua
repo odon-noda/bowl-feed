883dbbde752bbf463663c672dd167a32fda53f23
Polynomialdivision <vincent@systemli.org>
2019-05-28 11:40:19 +0200

    add signature

--- luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
+++ luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
@@ -5,6 +5,8 @@ s = m:section(NamedSection, "__networkoverview__")
 
 function s.render(self, sid)
 	local tpl = require "luci.template"
+	local json = require "luci.json"
+	local utl = require "luci.util"
 	tpl.render_string([[
 		<ul>
 			<%
@@ -31,7 +33,19 @@ function s.render(self, sid)
 					<%
 					local mac2, data2
 					for mac2, data2 in pairs(data) do					
+						local devicename
 						local host = mac2 and hosts[mac2]
+						local httpclient = require('luci.httpclient')
+						if not(data2.signature == nil) then
+							local key = "3f571d11466714f41d72bc9aac30858b80d36fa4"
+							local url = "http://wifiscout.inet.tu-berlin.de/api/v1/device/?key=" .. key .. "&signature=" .. data2.signature
+							local rc, r, txt, s = httpclient.request_raw(url)
+							local json = require "luci.json"
+							local req = json.decode(txt)
+							if not(req["results"] == nil) then
+								devicename = req["results"][1]["name"]
+							end
+						end
 					%>
 						<li>
 						<strong>Client: <%= mac2 %><br />
@@ -41,6 +55,8 @@ function s.render(self, sid)
 			                        <strong>Frequency is: </strong><%= "%.3f" %( data2.freq / 1000 ) %> GHz (Channel: <%= "%d" %( status.frequency_to_channel(data2.freq) ) %>)<br />
 						<strong>HT: </strong><%= (data2.ht == true) and "available" or "not available" %><br />
 						<strong>VHT: </strong><%= (data2.vht == true) and "available" or "not available" %><br />
+						<strong>Signature: </strong><%= "%s" %data2.signature %><br />
+						<strong>Devicename: </strong><%= "%s" %devicename %><br />
 						<!--
 						<strong>Signal is: </strong><%= "%d" %data2.signal %><br />
 						<strong>Channel Utilization is: </strong><%= "%d" %data2.channel_utilization %><br />
